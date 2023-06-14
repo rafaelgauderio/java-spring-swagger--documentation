142f7de85469f690d532386dee5d6cafe181309e
Rafael de Luca <deluca1712@gmail.com>
2023-06-14 16:20:58 -0300

    controller - insert, update, delete

--- backend/src/main/java/rafaeldeluca/com/movie/controllers/MovieController.java
+++ backend/src/main/java/rafaeldeluca/com/movie/controllers/MovieController.java
@@ -3,13 +3,14 @@ package rafaeldeluca.com.movie.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import rafaeldeluca.com.movie.entities.dto.MovieDTO;
 import rafaeldeluca.com.movie.services.MovieService;
+
+import java.net.URI;
 
 //Controlador vai implementar um endpoint e responder uma página de movie
 
@@ -29,6 +30,24 @@ public class MovieController {
 	public MovieDTO findById(@PathVariable Long id) {
 		return service.findById(id);
 	}
-	
+
+	@PostMapping
+	public ResponseEntity<MovieDTO> insert(@RequestBody MovieDTO movieDTO) {
+		movieDTO = service.insert(movieDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(movieDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(movieDTO);
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<MovieDTO> update (@RequestBody MovieDTO movieDTO,@PathVariable Long id) {
+		movieDTO = service.update(movieDTO, id);
+		return ResponseEntity.ok().body(movieDTO);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<MovieDTO> delete (@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
 
 }
