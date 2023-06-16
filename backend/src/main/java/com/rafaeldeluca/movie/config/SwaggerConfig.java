cb496f7bff619821e61219bf9809579ca88618f9
Rafael de Luca <deluca1712@gmail.com>
2023-06-16 16:21:15 -0300

    Incluindo opcao Authorize no swagger

--- backend/src/main/java/com/rafaeldeluca/movie/config/SwaggerConfig.java
+++ backend/src/main/java/com/rafaeldeluca/movie/config/SwaggerConfig.java
@@ -3,26 +3,49 @@ package com.rafaeldeluca.movie.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 @Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
 
+    private static final String AUTHORIZATION_HEADER="Authorization";
+
+    private ApiKey apiKeys () {
+        return new ApiKey("JWT",AUTHORIZATION_HEADER,"header");
+    }
+
+    private List<SecurityContext> securityContextMethod () {
+        return Arrays.asList(SecurityContext.builder().securityReferences(securityReferenceMethod()).build());
+    }
+
+    private List<SecurityReference> securityReferenceMethod() {
+        // definindo o scopo
+        AuthorizationScope globalScope = new AuthorizationScope("global", "accessEverything");
+        return Arrays.asList(new SecurityReference("JWT", new AuthorizationScope[]{globalScope}));
+    }
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(securityContextMethod())
+                .securitySchemes(Arrays.asList(apiKeys()))
                 .select()
                 //.apis(RequestHandlerSelectors.basePackage(".com.rafaeldeluca.movie.controllers"))
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
