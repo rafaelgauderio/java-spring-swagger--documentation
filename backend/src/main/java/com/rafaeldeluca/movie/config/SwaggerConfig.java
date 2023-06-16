a6ad9c109e7697860cb466e83e47a15f67ebae69
Rafael de Luca <deluca1712@gmail.com>
2023-06-16 15:38:23 -0300

    Metadados da aplicação

--- backend/src/main/java/com/rafaeldeluca/movie/config/SwaggerConfig.java
+++ backend/src/main/java/com/rafaeldeluca/movie/config/SwaggerConfig.java
@@ -2,16 +2,22 @@ package com.rafaeldeluca.movie.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.RestController;
 
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfig {
 
     @Bean
@@ -21,6 +27,13 @@ public class SwaggerConfig {
                 //.apis(RequestHandlerSelectors.basePackage(".com.rafaeldeluca.movie.controllers"))
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo metaDados () {
+        return new ApiInfoBuilder().title("API de avaliação de filmes")
+                .description("\"Spring Boot REST API para avaliar filmes. Dodumentação com swagger\"").version("1.0.0")
+            .contact(new Contact("Rafael de Luca","https://github.com/rafaelgauderio","deluca1712@gmail.com"))
                 .build();
     }
 }
